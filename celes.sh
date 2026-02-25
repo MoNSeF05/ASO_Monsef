2c34d1a1592d855061e0a325f556c95d4d584718
MoNSeF05 <benslaimanhajajmonsef@gmail.com>
2026-02-25 17:40:39 +0100

    Delete celes.sh

--- celes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-userdel -r Maria >/dev/null
-useradd -m -s /bin/bash Celes
-cp -r /home/Maria/* /home/Celes >/dev/null
-mv /home/Celes/Maria.txt /home/Celes/Celes.txt >/dev/null
-useradd -m Locke
-echo "Celes y Locke han sido creados"
